fc9c6cfc24a0fe16390795ecd17b8672d5612d64
venkatesh-ididala <venkateshididala@gmail.com>
2024-12-16 15:16:53 +0530

    change content

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,6 +4,7 @@ import ResourceCard from '@/components/ResourceCard';
 import SearchForm from '@/components/SearchForm';
 import { getResources, getResourcesPlaylist } from '@/sanity/actions';
 
+
 export const revalidate = 900;
 
 // Define TypeScript interfaces
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-1 max-md:flex-col'>
-      <p>Copyright 2023 JS Mastery Pro | ALL Rights reserved</p>
+      <p>Copyright © 2024 VENKATESH  | All Rights reserved</p>
       <div className='flex gap-x-9'>
         <Link href="/terms-of-use">
         Terms & conditions
