d96b9d8dc378e0f322cb20c118f92f96aec4aa17
venkatesh-ididala <venkateshididala@gmail.com>
2024-12-16 15:06:11 +0530

    icon setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white">
       <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
         <Link href="/">
-        <Image src="/json-logo.svg" alt="logo"
+        <Image src="/jsm-logo.svg" alt="logo"
         width={55}
         height={40}
         />
